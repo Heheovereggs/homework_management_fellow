692226000704f24e2e9ff188f8ad8fa624c95ed9
吳羽陽 <wuyuyang43@gmail.com>
2021-02-07 10:52:48 -0500

    slide animation

--- lib/screens/activation_pending_screen.dart
+++ lib/screens/activation_pending_screen.dart
@@ -17,13 +17,13 @@ class _ActivationPendingScreenState extends State<ActivationPendingScreen>
     with SingleTickerProviderStateMixin {
   AnimationController controller;
   Animation<Offset> offset;
-  bool isActivate;
+  bool isActivate = false;
 
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
 
     offset = Tween<Offset>(begin: Offset.zero, end: Offset(0.0, 1.0)).animate(controller);
   }
@@ -40,78 +40,93 @@ class _ActivationPendingScreenState extends State<ActivationPendingScreen>
       final prefs = await SharedPreferences.getInstance();
       String email = prefs.getString('email');
       String uid = prefs.getString('uid');
-      Student student = await Provider.of<FirebaseService>(context, listen: false)
-          .checkUser(email: email, uid: uid);
+      Student student =
+          await Provider.of<FirebaseService>(context, listen: false).checkUser(email: email, uid: uid);
       return student.activate;
     }
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Text(
-            "Activation pending",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              foreground: Paint()..shader = shader2,
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Text(
-            "PLease check later or press the button to refresh status",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              foreground: Paint()..shader = shader1,
-            ),
-          ),
-          SizedBox(
-            height: 35,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(19),
-            child: SizedBox(
-              width: 180,
-              child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  color: Color(0xFF2196f3),
-                  textColor: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Refresh',
-                      style: TextStyle(
-                        fontSize: 18,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Activation pending",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()..shader = shader2,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "PLease check later or press the button to refresh status",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()..shader = shader1,
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(19),
+                child: SizedBox(
+                  width: 180,
+                  child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
-                    ),
-                  ),
-                  onPressed: () async {
-                    isActivate = await checkUserStatus();
-                    if (isActivate == false) {
-                      controller.reverse();
-                      Future.delayed(Duration(seconds: 3), () {
-                        controller.forward();
-                      });
-                    } else {
-                      Navigator.pushNamed(context, TaskScreen.id);
-                    }
-                  }),
-            ),
+                      color: Color(0xFF2196f3),
+                      textColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Refresh',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      onPressed: () async {
+//                    isActivate = await checkUserStatus();
+                        if (isActivate == false) {
+                          controller.reverse();
+                          Future.delayed(Duration(seconds: 2), () {
+                            controller.forward();
+                          });
+                        } else {
+                          Navigator.pushNamed(context, TaskScreen.id);
+                        }
+                      }),
+                ),
+              ),
+            ],
           ),
           Align(
             alignment: Alignment.bottomCenter,
             child: SlideTransition(
               position: offset,
               child: Padding(
-                padding: EdgeInsets.all(50.0),
+                padding: EdgeInsets.only(bottom: 80.0),
                 child: Container(
-                  child: Text('failed'),
+                  width: 175,
+                  height: 45,
+                  decoration:
+                      BoxDecoration(color: Color(0xCC000000), borderRadius: BorderRadius.circular(10.0)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                        child: Text(
+                      "Didn't activated yet...",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ),
                 ),
               ),
             ),
